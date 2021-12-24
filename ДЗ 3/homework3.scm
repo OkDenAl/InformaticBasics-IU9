34cc383dc59e71aaa73f8d813a6c7ad7c73a4746
Denis Okutin <94537822+OkDenAl@users.noreply.github.com>
2021-12-24 13:00:05 +0300

    Upd

---
+++ ДЗ 3/homework3.scm
@@ -0,0 +1,40 @@
+(define (sign expr)
+  (cond
+    ((equal? (car expr) '-) '-)
+    ((equal? (car expr) '+) '+)
+    (else '())))
+
+(define (helper1 expr)
+  (cond
+    ((number? expr) 0)
+    ((equal? expr 'x)1)
+    ((equal? expr '-x)-1)))
+
+(define (derivative expr)
+  (if (not(list? expr))
+          (derivative (list expr))
+      (cond
+        ((< (length expr) 2) (helper1 (car expr)))
+        ((and (equal? (car expr) '*) (number? (cadr expr)))
+         (if (< (length expr) 4)
+             (list '* (cadr expr) (derivative (caddr expr)))
+             (list '* (cadr expr) (derivative (cons '* (cddr expr))))))
+        ((and (equal? (car expr) 'expt) (equal?  (cadr expr) 'x))
+         (list '* (caddr expr) (list (car expr) (cadr expr) (- (caddr expr) 1))))
+        ((and (equal? (car expr) 'expt) (number? (cadr expr))) (list '* expr (list 'log (cadr expr))))
+        ((equal? (car expr) 'cos) (list '* (list '-sin (cadr expr)) (derivative (cadr expr))))
+        ((equal? (car expr) 'sin) (list '* (list 'cos (cadr expr)) (derivative (cadr expr))))
+        ((equal? (car expr) 'exp) (list '* (list 'exp (cadr expr)) (derivative (cadr expr))))
+        ((equal? (car expr) 'log) (list '/(derivative (cadr expr)) (cadr expr)))
+        ((equal? (car expr) '/) (list '/ (list '- (list '* (derivative (cadr expr)) (caddr expr))
+                                               (list '* (cadr expr) (derivative(caddr expr)))) (list 'expt (caddr expr) 2)))
+        ((equal? (car expr) '*) (list '+ (list '* (derivative (cadr expr)) (caddr expr))
+                                      (list '* (cadr expr) (derivative (caddr expr)))))
+        ((not(null? (sign expr))) (list (sign expr) (derivative (cadr expr)) (derivative (caddr expr)))))))
+
+    
+  
+    
+
+
+
---
+++ ДЗ 4/homework4.scm
@@ -0,0 +1,117 @@
+(use-syntax (ice-9 syncase))
+
+(define (memoized-factorial n)
+  (let ((known-results '()))
+    (define (loop n)
+      (cond
+        ((<= n 1) 1)
+        (else(let((res (assoc n known-results)))
+               (if res
+                   (cadr res)
+                   (begin
+                     (set! res (* (loop(- n 1)) n))
+                     (set! known-results (cons (cons n res) known-results))
+                     res))))))
+    (loop n)))
+
+;2------------------------------------------------------------------
+
+(define-syntax lazy-cons
+  (syntax-rules ()
+    ((lazy-cons a b)
+     (cons a (delay b)))))
+
+(define (lazy-car p)
+  (car p))
+
+(define (lazy-cdr p)
+  (force (cdr p)))
+
+(define (lazy-head xs k)
+   (if (= k 0)
+       '()
+       (cons (lazy-car xs) (lazy-head (lazy-cdr xs) (- k 1)))))
+
+(define (lazy-ref xs k)
+  (define (loop xs n)
+    (if (= n (- k 1))
+        (lazy-car xs)
+        (loop (lazy-cdr xs) (+ n 1))))
+  (loop xs 0))
+
+(define (naturals start)
+  (lazy-cons start (naturals (+ start 1))))
+
+(define (fact)
+  (define (loop n p)
+      (lazy-cons  (* n p) (loop (+ n 1) (* n p))))
+  (loop 1 1))
+
+(define (lazy-factorial n)
+  (lazy-ref (fact) n))
+
+;3------------------------------------------------------------------
+
+(define (read-words file)
+  (let ((x (open-input-file file)))
+    (define (loop c word ans)
+      (if (eof-object? (peek-char x))
+          (begin
+            (close-input-port x)
+            (if (> c 0)
+                (reverse (cons word ans))
+                (reverse ans)))
+          (let ((char (read-char x)))
+            (if (char-whitespace? char)
+                (if (> c 0)
+                    (loop 0 "" (cons word ans))
+                    (loop 0 "" ans))
+                (loop (+ c 1) (string-append word (string char)) ans)))))
+    (loop 0 "" '())))
+
+;4------------------------------------------------------------------
+
+(define ie (interaction-environment))
+
+(define-syntax define-struct
+  (syntax-rules()
+    ((define-struct name (f_n1 ...))
+     (begin
+       (eval (list 'define (string->symbol (string-append "make-" (symbol->string 'name)))
+                   (lambda (f_n1 ...)
+                     `(,(list 'struct 'name) ,(list 'f_n1 f_n1) ...)))ie)
+       (eval (list 'define (string->symbol (string-append (symbol->string 'name) "?"))
+                   (lambda (var)
+                     (and (list? var) (not (null? var)) (equal? (caar var) 'struct) (equal? (cadar var) 'name)))) ie)
+       (eval (list 'define (string->symbol (string-append (symbol->string 'name) "-" (symbol->string 'f_n1)))
+                   (lambda (var)
+                     (let ((res (assoc 'f_n1 (cdr var))))
+                       (and res (cadr res))))) ie) ...
+       (eval (list 'define (string->symbol (string-append "set-" (symbol->string 'name) "-" (symbol->string 'f_n1) "!"))
+                   (lambda (var value)
+                       (and (list? var) (not (null? var))(set-car! (cdr (assoc 'f_n1 (cdr var))) value)))) ie) ...))))
+
+
+
+
+
+(define-syntax define-data
+  (syntax-rules()
+    ((define-data name ((f_n1 par ...) ...))
+     (begin
+       (eval (list 'define 'f_n1
+                   (lambda (par ...)
+                     `(,(list 'data 'name),(list 'type 'f_n1) ,(list 'par par) ...)))ie) ...
+       (eval (list 'define (string->symbol (string-append (symbol->string 'name) "?"))
+                   (lambda (var)
+                     (and (list? var) (not (null? var)) (equal? (caar var) 'data)
+                          (equal? (cadar var) 'name)))) ie)))))
+
+(define-syntax match
+  (syntax-rules()
+    ((match var ((f_n1 par ...) expr) ...)
+     (cond
+       ((equal? (cadadr var) 'f_n1)
+        (let ((par (cadr(assoc 'par var))) ...) expr))
+       ...
+       (else var)))))
